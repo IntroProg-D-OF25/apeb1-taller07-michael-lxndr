6707f199d62f64123ea7a88fae68298d2d43fe5f
Michael Alexander <michael.lxndr@outlook.com>
2024-11-25 01:06:07 -0500

    Resolución del Problema 5 y 6

--- Proyecto_Taller-7/src/Problem5_IngresarEstudiantes.java
+++ Proyecto_Taller-7/src/Problem5_IngresarEstudiantes.java
@@ -1,13 +1,34 @@
 /*
  * Generar un programa Java que permita ingresar 4 estudiantes; por cada uno de ellos ingresar el nombre del estudiante, el promedio de ciclo. Presentar el siguiente reporte
- * Estudiante    1    10      Aprobado
- * Estudiante    2    6.9     Reprobado
- * Estudiante    3    7       Aprobado
- * Estudiante    4    5       Reprobado
+ Estudiante1 10 Aprobado
+ Estudiante2 6.9 Reprobado
+ Estudiante3 7  Aprobado
+ Estudiante4 5  Reprobado
  * Atención; con base al valor del promedio, usted debe asignar en cada registro el tipo Aprobado o Reprobado.
  */
 
+import java.util.Scanner;
+
 public class Problem5_IngresarEstudiantes {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String nombre, pasa = "Reprobado", out = "";
+        float promedio;
+        int i = 1, j = 4;
+
+        while (i <= j) {
+            System.out.println("Ingrese el nombre del estudiante:   (" + i + "/" + j + ")");
+            nombre = sc.next();
+            System.out.println("Ingrese el promedio del estudiante " + i + ":");
+            promedio = sc.nextFloat();
+            if (promedio >= 7) {
+                pasa = "Aprobado";
+            }
+
+            out += nombre + "\t\t" + promedio + "\t\t\t" + pasa + "\n";
+            i++;
+        }
+
+        System.out.print(out);
     }
 }
--- Proyecto_Taller-7/src/Problem6_VentaDescuento.java
+++ Proyecto_Taller-7/src/Problem6_VentaDescuento.java
@@ -2,8 +2,8 @@
  * Una empresa de comercialización de computadoras realiza el proceso de venta haciendo un descuento por tipo de cliente:
  * Si es cliente tipo 1 hay un descuento del 10%
  * Si es cliente tipo 2 hay un descuento del 20%
- * En caso que sea otro tipo de cliente, no hay descuento.
- * Generar un proceso que permita ingresar 7 ventas: por cada venta preguntar los siguiente datos:
+ * En caso de que sea otro tipo de cliente, no hay descuento.
+ * Generar un proceso que permita ingresar 7 ventas: por cada venta preguntar los siguientes datos:
  * Nombre del cliente
  * Costo de la computadora (solo se vende una computadora por transacción)
  * Tipo de cliente
@@ -11,8 +11,31 @@
  * Cliente tipo 1, compra computadora con precio $100
  */
 
+import java.util.Scanner;
+
 public class Problem6_VentaDescuento {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String cliente, tipoCli, out = "";
+        double costoPC, descuento;
+        int i = 1, j = 7;
 
+        while (i <= j) {
+            System.out.println("Ingrese el nombre del cliente: ");
+            cliente = sc.next();
+            System.out.println("Ingrese el costro de la computadora: ");
+            costoPC = sc.nextDouble();
+            System.out.println("Ingrese el tipo de cliente:    (1||2)");
+            tipoCli = sc.next();
+
+            descuento = costoPC * 0.9;
+            if (tipoCli.equals("2")) {
+                descuento = costoPC * 0.8;
+            }
+            out += cliente + "\t\t\t\t\t\t\tTipo " + tipoCli + "\t\t\t\t\t$" + descuento + "\n";
+            i++;
+        }
+        System.out.print("Nombre del cliente: " + "\t\t" + "Tipo de cliente" + "\t\t\t" + "Precio con descuento\n");
+        System.out.print(out);
     }
 }
