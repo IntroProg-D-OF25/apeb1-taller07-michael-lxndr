a3c026ca4219f6af3523b6e2b4f8d121ce17849c
Michael Alexander <michael.lxndr@outlook.com>
2024-11-25 13:48:08 -0500

    Resolución del Problema 7

--- Proyecto_Taller-7/src/Problem7_GenereImprima.java
+++ Proyecto_Taller-7/src/Problem7_GenereImprima.java
@@ -2,8 +2,29 @@
  * Generar e imprimir la siguiente serie:
  * sumatoria = -(1/1)+(1/2)-(1/3)+(1/4)-(1/5)+(1/6)-(1/7)+(1/8)-(1/9)+(1/10)
  */
+
 public class Problem7_GenereImprima {
     public static void main(String[] args) {
+        String output = "", signo;
+        int i = 0, j = 10;
 
+        while (i < j) {
+            i++;
+
+            if (i % 2 == 0) {
+                signo = "+";
+            } else {
+                signo = "-";
+            }
+
+            output += signo + "(" + 1 + "/" + i + ")";
+        }
+
+        System.out.println("sumatoria = " + output);
     }
 }
+
+/*
+ * Salida de resultados
+ * sumatoria = -(1/1)+(1/2)-(1/3)+(1/4)-(1/5)+(1/6)-(1/7)+(1/8)-(1/9)+(1/10)
+ */
